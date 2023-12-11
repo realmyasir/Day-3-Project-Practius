0b2b279268c564811fe3c3b08969481dde19361a
Muhammad Yasir <145558820+realmyasir@users.noreply.github.com>
2023-12-11 14:28:17 +0500

    Add files via upload
    Like 7 Exapmle for Python Learning

---
+++ Practius.py
@@ -0,0 +1,100 @@
+# print('welcome to the rollercoaster')
+# height=int(input('what is your height in cm?'))
+# bill=0
+# if height>120:
+#     print('you can ride the rollercoaster')
+#     age=int(input('Enter your Age'))
+#     if age<12:
+#         bill=5
+#         print('Please Pay 5 $ ticket')    
+#     elif age<=18:
+#         bill=7
+#         print('Please Pay 7$ Ticket')
+#     else:
+#         bill=12
+#         print('Please pay 12$ ticket')
+#     wantstobill=input('You are taken an photo Y/N')
+#     bill=bill+3
+#     print(f'Your Final Bill is {bill}')
+# else:
+#     print('sorry,you ave to grow taller before you can ride')
+
+# number=int(input('Enter your Number'))
+# if number%2==0:
+#     print('You Enter Even Number ')
+# else:
+#     print('You Enter Odd Number')
+
+# height=float(input('Enter Your Height'))
+# weight=int(input('Enter your weight'))
+# bmi=weight/height**2
+# if bmi <18.5:
+#     print(f'your bmi is {bmi} you are under weight ')
+# elif bmi <25.0:
+#     print(f'your bmi is {bmi} you are normal weight ')
+# elif bmi < 30.0:
+#     print(f'your bmi is {bmi} you are Slightly weight ')
+# elif bmi <35:
+#     print(f'your bmi is {bmi} you are absees ')
+# else:
+#     print(f'your bmi is {bmi} you are clinicaly abese ')
+    
+# year=int(input('enter you Year and this is leap year or not'))
+# if year%4==0:
+#     if year%100==0:
+#         if year%400==0:
+#             print('leap Year')
+#         else:
+#             print('Not Leap Year')
+#     else:
+#         print('Leap Year')
+# else:
+#     print('not Leap Year')
+
+
+# print('Thank You For Choosing Pizza Deliveries')
+# size=input()
+# add_peperoni=input()
+# extra_chees=input()
+# bill=0
+# if size=='s':
+#     bill+=15
+# elif size=='m':
+#     bill+=20
+# else:
+#     bill+=25
+# if add_peperoni =='y':
+#     if size=='s':
+#      bill+=2
+#     else:
+#         bill+=3
+        
+# print(f'Your Final Price Of pizza{bill}')
+
+
+# Love Calculator
+
+# name1=input('Enter Your Name>>')
+# name2=input('Enter Your Name>>')
+# combined_name=name1+name2
+# lower_name=combined_name.lower()
+# t=lower_name.count('t')
+# r=lower_name.count('r')
+# u=lower_name.count('u')
+# e=lower_name.count('e')
+# first_digit=t+r+u+e
+# l=lower_name.count('l')
+# o=lower_name.count('o')
+# v=lower_name.count('v')
+# e=lower_name.count('e')
+# second_digit=l+o+v+e
+# score=int(str(first_digit)+str(second_digit))
+
+# if(score<10)or(score>90):
+#     print(f"Your Score is{score} to gettogether like Coke And Mentos")
+# elif (score >= 40) and (score <= 50):
+#     print(f"Your Score is{score} Get together")
+# else:
+#     print(f'Your Score is {score}')
+    
+    
